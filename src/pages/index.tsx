3b8c86cc53a178163ee42f632875669d8e4b3410
Diego <diegogoulart.aws@gmail.com>
2022-02-20 21:04:53 -0300

    add home html and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Home | dev.News</title>
       </Head>
-      <h1>
-        hello <span>world</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br />the <span>React</span> world.</h1>
+          <p>Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/mulher.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
